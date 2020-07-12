708efd4f8cd462bae02661ff0a362edf10f5e031
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-07-12 15:22:14 -0500

    Center content

--- clicky/src/components/Header.js
+++ clicky/src/components/Header.js
@@ -3,8 +3,8 @@ import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1 className={styles.header}>Clicky Game: Plant Edition</h1>
+    <div className={styles.header}>
+      <h1>Clicky Game: Plant Edition</h1>
       <p>Try not to click the same image TWICE!</p>
     </div>
   );
--- clicky/src/components/Plant.js
+++ clicky/src/components/Plant.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from '../styles/ImageCard.module.css'
 
 const Plant = (props) => {
   console.log(props);
   return (
     <div>
-      <img src={props.image} alt={props.alt}/>
+      <img className={styles.card} src={props.image} alt={props.alt}/>
     </div>
   );
 };
