5fdeb754b3de583c7e88f90adda8e511241f7a5a
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-07-22 09:12:26 -0500

    Set states and pass in props

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -8,31 +8,17 @@ class App extends Component {
   state = {
     cards,
     score: 0,
-    highscore: 0
+    highscore: 0,
+    clickedCards: []
   };
-
-  // Grab the img by id & if clicked, set click key to 1 & increase score by 1 
-  handleClick = id => {
-    this.state.cards.find((card, i) => {
-      if (card.id === id) {
-        if (cards[i].clicked === 0) {
-          cards[i].clicked = cards[i].clicked + 1;
-          this.setState({ score: this.state.score + 1 });
-        }
-        this.state.cards.sort(() => Math.random() - 0.5)
-        return true; 
-      }
-    })
-    console.log('Current score:', this.state.score)
-  }
 
   render() {
     return (
       <Wrapper>
-        <Header />
+        <Header score={this.state.score} highscore={this.state.highscore}/>
           {this.state.cards.map(card => (
             <Card 
-              clicked={this.clicked} 
+              handleClick={this.handleClick}
               list={cards}
               key={card.id} 
               image={card.image} 
